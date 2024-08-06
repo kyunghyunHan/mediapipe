ed88357a70fa14a279b9b735ada9e158210f41c2
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-08-06 14:11:28 +0900

    face_detection

--- blaze_face.py
+++ blaze_face.py
@@ -1,9 +1,20 @@
 import cv2 as cv
 import mediapipe as mp
 
-cap = cv.VideoCapture(0)
-
+# cap = cv.VideoCapture(0)
+img =cv.imread('face.jpeg')
 
 mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
 
+face_detection =mp_face_detection.FaceDetection(model_selection=1,min_detection_confidence=0.5)
+res=face_detection.process(cv.cvtColor(img,cv.COLOR_BGR2RGB))
+if not res.detections:
+    print("얼굴을 검출할수가 없습니다")
+else:
+    for detection in res.detections:
+        mp_drawing.draw_detection(img,detection)
+    cv.imshow("face",img)
+cv.waitKey()
+cv.destoryAllWindows()
 print(mp_face_detection)
